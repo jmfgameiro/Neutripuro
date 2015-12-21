8aca9bd5eca76a79b75e55fd008c960b70ea54cc
João Gameiro <jmfgameiro@gmail.com>
2015-12-21 11:31:24 +0000

    Animation Support

--- src/main/resources/js/index.js
+++ src/main/resources/js/index.js
@@ -5,12 +5,42 @@
 /***************************************************************************/
 
 $( function() {
-	$( '#home' ).css({ height: $( window ).innerHeight() });
+    $( '#footerYear' ).text( new Date().getFullYear() );
+    
+    var v_height = $( window ).innerHeight();
+    if( $( '#home' ).height > v_height )
+        v_height = $( '#home' ).height;
+    
+	$( '#home' ).css({ height: v_height });
 	$( window ).resize( function() {
-		$( '#home' ).css({ height: $( window ).innerHeight() });
+		$( '#home' ).css({ height: v_height });
 	});
     
-    
-    $( '#footerYear' ).text( new Date().getFullYear() );
+    if ( supports('textShadow') )
+        $( '#cssanimations' ).text( 'true' );
+    else
+        $( '#cssanimations' ).text( 'false' );
 });
 
+var supports = ( function() {
+   var div = document.createElement( 'div' ),
+      vendors = 'Khtml Ms O Moz Webkit'.split( ' ' ),
+      len = vendors.length;
+ 
+   return function( prop ) {
+      if ( prop in div.style ) return true;
+ 
+      prop = prop.replace(/^[a-z]/, function( val ) {
+         return val.toUpperCase();
+      });
+ 
+      while( len-- ) {
+         if ( vendors[len] + prop in div.style ) {
+            // browser supports box-shadow. Do what you need.
+            // Or use a bang (!) to test if the browser doesn't.
+            return true;
+         } 
+      }
+      return false;
+   };
+})();
