ab8cac73ebcc1fb3295631e7d12c309394659688
João Gameiro <jmfgameiro@gmail.com>
2016-01-06 10:35:56 +0000

    Google Analytics 2

--- src/main/resources/js/googleanalytics.js
+++ src/main/resources/js/googleanalytics.js
@@ -1,5 +1,4 @@
-<script>
-  ( function( i, s, o, g, r, a, m) {
+( function( i, s, o, g, r, a, m) {
     i[ 'GoogleAnalyticsObject' ] = r;
     i[ r ] = i[ r ] || function() { ( i[ r ].q = i[ r ].q || [] ).push( arguments ) },
         i[ r ].l = 1 * new Date() ; a = s.createElement( o ),
@@ -8,7 +7,6 @@
     a.src = g;
     m.parentNode.insertBefore( a, m )
   } )
-  ( window, document, 'script', '//www.google-analytics.com/analytics.js','ga');
-  ga( 'create', 'UA-72086723-1', 'auto' );
-  ga( 'send', 'pageview' );
-</script>
+( window, document, 'script', '//www.google-analytics.com/analytics.js','ga');
+ga( 'create', 'UA-72086723-1', 'auto' );
+ga( 'send', 'pageview' );
