d54228e102f93fe6bc9e8f9ebdb8aa0ce02d1461
João Gameiro <gameiro88@hotmail.com>
2016-03-07 23:05:00 +0000

    Nova Informação Fase 1

--- src/main/resources/js/sobrenos.js
+++ src/main/resources/js/sobrenos.js
@@ -6,10 +6,10 @@
 
 function body_load() {
     $( '#footerYear' ).text( new Date().getFullYear() );
-    $( '#quemsomos' ).css({ height: $( window ).innerHeight() - 50 });
+    $( '#sobrenos' ).css({ height: $( window ).innerHeight() - 50 });
 }
 
 $( window ).resize( function() {
-    $( '#quemsomos' ).css({ height: $( window ).innerHeight() - 50 });
+    $( '#sobrenos' ).css({ height: $( window ).innerHeight() - 50 });
 });
 
