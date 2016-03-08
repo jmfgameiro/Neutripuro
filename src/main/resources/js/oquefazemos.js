fb74ecd5bee6bff7f9d59a9e786b4e57489c5a77
João Gameiro <gameiro88@hotmail.com>
2016-03-08 02:48:12 +0000

    Correções 3

--- src/main/resources/js/oquefazemos.js
+++ src/main/resources/js/oquefazemos.js
@@ -8,12 +8,12 @@ function body_load() {
     $( '#footerYear' ).text( new Date().getFullYear() );
     var navbarHeight = $( '#navbarIncluded' ).height();
     $( '#oquefazemos' ).css( { height: $( window ).innerHeight() - navbarHeight } );
-    $( '.featurette-divider' ).css( "margin-top": navbarHeight );
+    $( '.featurette-divider' ).css( 'margin-top': navbarHeight );
 }
 
 $( window ).resize( function() {
     var navbarHeight = $( '#navbarIncluded' ).height();
     $( '#oquefazemos' ).css( { height: $( window ).innerHeight() - navbarHeight } );
-    $( '.featurette-divider' ).css( "margin-top": navbarHeight );
+    $( '.featurette-divider' ).css( 'margin-top': navbarHeight );
 });
 
