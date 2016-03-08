26263518e1cd222227e83b3ac9c62feb1b06d7c4
João Gameiro <gameiro88@hotmail.com>
2016-03-08 11:06:19 +0000

    Nova Informação Fase 5

--- src/main/resources/js/contatos.js
+++ src/main/resources/js/contatos.js
@@ -12,3 +12,26 @@ function body_load() {
 $( window ).resize( function() {
     $( '#contatos' ).css( { height: $( window ).innerHeight() - $( '#navbarIncluded' ).height() } );
 });
+
+function initialize() {
+	//
+	var position = new google.maps.LatLng( 39.426260, -9.161524 );
+	var positionA8 = new google.maps.LatLng( 39.419033, -9.152726 );
+	
+	//
+	var mapOptions = {
+			center: position,
+			zoom: 14,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	var map = new google.maps.Map( document.getElementById( "map-container" ), mapOptions );
+	
+	//
+	var marker = new google.maps.Marker( { position: position, map: map, title: 'Neutripuro', label: 'Neutripuro' } );
+	var bounds = new google.maps.LatLngBounds();
+	bounds.extend( position );
+	bounds.extend( positionA8 );
+	map.fitBounds( bounds );
+}
+
+google.maps.event.addDomListener( window, 'load', initialize );
