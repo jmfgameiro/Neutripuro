cbbb11c2f88a8b27a87116a971e447c9e729e3d7
João Gameiro <gameiro88@hotmail.com>
2016-03-08 00:03:15 +0000

    correções 3

--- src/main/resources/js/navbar.js
+++ src/main/resources/js/navbar.js
@@ -1,5 +1,3 @@
 $( function(){
-    var getImport = $( '#navbar-file' );
-    var getContent = getImport.import.querySelector( '#navbar-container' );
-    $( "#navbarIncluded" ).html( document.importNode( getContent, true ) ); 
+    $( "#navbarIncluded" ).load( '/navbar.html' )
 } );
