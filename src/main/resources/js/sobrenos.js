acfd3539db3e17b6f8d00d03b7625055897e0d8b
João Gameiro <gameiro88@hotmail.com>
2016-03-08 02:06:55 +0000

    Correções 1

--- src/main/resources/js/sobrenos.js
+++ src/main/resources/js/sobrenos.js
@@ -7,9 +7,11 @@
 function body_load() {
     $( '#footerYear' ).text( new Date().getFullYear() );
     $( '#sobrenos' ).css( { height: $( window ).innerHeight() - $( '#navbarIncluded' ).height() } );
+    $( '#featurette-divider' ).css( { margin-top: $( '#navbarIncluded' ).height() } );
 }
 
 $( window ).resize( function() {
     $( '#sobrenos' ).css( { height: $( window ).innerHeight() - $( '#navbarIncluded' ).height() } );
+    $( '#featurette-divider' ).css( { margin-top: $( '#navbarIncluded' ).height() } );
 });
 
