6257e8b60972e1238b239f75d43eb2bfab3bd2fc
João Gameiro <gameiro88@hotmail.com>
2016-03-08 12:08:50 +0000

    Teste 4 Correções 1

--- src/main/resources/js/contatos.js
+++ src/main/resources/js/contatos.js
@@ -6,11 +6,11 @@
 
 function body_load() {
     $( '#footerYear' ).text( new Date().getFullYear() );
-    $( '#contatos' ).css( { height: $( window ).innerHeight() - $( '#navbarIncluded' ).height() } );
+    //$( '#contatos' ).css( { height: $( window ).innerHeight() - $( '#navbarIncluded' ).height() } );
 }
 
 $( window ).resize( function() {
-    $( '#contatos' ).css( { height: $( window ).innerHeight() - $( '#navbarIncluded' ).height() } );
+    //$( '#contatos' ).css( { height: $( window ).innerHeight() - $( '#navbarIncluded' ).height() } );
 });
 
 function initialize() {
